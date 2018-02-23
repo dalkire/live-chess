a167980fcf1bbf05ed05ef76495f7703a9e41317
David Alkire <david.alkire@gmail.com>
2018-02-22 20:42:54 -0500

    Refactored but slow

--- board.rkt
+++ board.rkt
@@ -3,11 +3,42 @@
 (require racket/gui
          pict)
 
-(provide board draw-board)
+(provide pos->squares index->row-col)
 
 (define SQUARE-SIZE 100)
 (define SQUARES-PER-COL 8)
 (define SQUARES-PER-ROW 8)
+
+(define POS "RNBQKB RPPPP PPP     N      P      p        p   ppp  ppprnbqkbnr")
+
+(define piece-style "usual")
+(define padding 8)
+
+(define (char->piece ch)
+  (define base (string-append "./pieces/" piece-style "/" piece-style "_"))
+  (define suffix ".png")
+  (define color-char
+    (cond [(char-lower-case? ch) "w"]
+          [(char-upper-case? ch) "b"]
+          [else ""]))
+  (define piece-char (string (char-downcase ch)))
+  (define piece (string-append base color-char piece-char suffix))
+  (cond [(char-whitespace? ch) (filled-rectangle 0 0)]
+        [else (inset (bitmap piece) padding)]))
+
+
+;; Get a square with the proper piece (or none)
+;; given the board state and position (index in 64-element state string)
+(define (pos-sq state index)
+  (cc-superimpose (index->square index)
+                  (char->piece (string-ref state index))))
+
+;; From 64-char board state to list of composed squares
+(define (pos->squares state)
+  (define piece-chars (string->list state))
+  (build-list (length piece-chars)
+              (lambda (i)
+                (pos-sq state i))))
 
 (define (color->square color)
   (cond [(string=? color "white")
@@ -19,6 +50,10 @@
                     #:color (make-color 120 150 86)
                     #:draw-border? #f)]))
 
+;; Translate an index to the row-col values
+(define (index->row-col index)
+  (quotient/remainder index 8))
+
 (define (index->color index)
   (define-values (row col) (quotient/remainder index 8))
   (cond [(even? (+ row col)) "white"]
@@ -27,12 +62,11 @@
 (define index->square (compose color->square index->color))
 
 (define (draw-board p)
-  (define-values (width height) (send p get-size))
+  ;; (define-values (width height) (send dc get-size))
+  (define width (send p min-width))
+  (define height (send p min-height))
   (define board-size (cond [(< width height) width]
                            [else height]))
-  (set! board-size 200)
-  (println (send p stretchable-width))
-  (println (send p stretchable-height))
   (define square-size (quotient board-size 8))
   (board p square-size))
 
--- gui.rkt
+++ gui.rkt
@@ -18,46 +18,36 @@
 
 (define main-panel (new horizontal-panel%
                         [parent frame]))
+
+;; (define my-on-size-panel% (class panel% (super-new)
+;;                             (define/override (on-size width height)
+;;                               (lambda ()
+;;                                 (print (send this get-width))
+;;                                 (print (send this get-height))))))
+
 (define board-panel (new panel%
                          [parent main-panel]
-                         [min-width 200]
-                         [min-height 200]))
+                         [min-width 800]
+                         [min-height 800]))
+;; (draw-board board-panel)
 
 (define control-panel (new vertical-panel%
                            [parent main-panel]
                            [min-width 400]
                            [stretchable-width #f]))
 
-(define (char->piece ch)
-  (define base (string-append "./pieces/" piece-style "/" piece-style "_"))
-  (define suffix ".png")
-  (define color-char
-    (cond [(char-lower-case? ch) "w"]
-          [(char-upper-case? ch) "b"]))
-  (define piece-char (string (char-downcase ch)))
-  (define piece (string-append base color-char piece-char suffix))
-  ;; (define piece (string-append piece-style "-" color-char piece-char))
-  ;; (inset (bitmap (eval (string->symbol piece))) padding)
-  (inset (bitmap piece) padding))
+;; (define (char->piece ch)
+;;   (define base (string-append "./pieces/" piece-style "/" piece-style "_"))
+;;   (define suffix ".png")
+;;   (define color-char
+;;     (cond [(char-lower-case? ch) "w"]
+;;           [(char-upper-case? ch) "b"]))
+;;   (define piece-char (string (char-downcase ch)))
+;;   (define piece (string-append base color-char piece-char suffix))
+;;   ;; (define piece (string-append piece-style "-" color-char piece-char))
+;;   ;; (inset (bitmap (eval (string->symbol piece))) padding)
+;;   (inset (bitmap piece) padding))
 
-(define black-sq
-  (filled-rectangle square-size square-size
-                    #:color (make-color 120 150 86)
-                    #:draw-border? #f))
-
-(define white-sq
-  (filled-rectangle square-size square-size
-                    #:color (make-color 239 237 209)
-                    #:draw-border? #f))
-
-;; (define (canvas-cb pieces sq-size white-sq black-sq)
-;;   (lambda (canvas dc)))
-
-(define (draw-square square dc x y)
-  (draw-pict square dc x y))
-
-(define (draw-piece piece dc x y)
-  (draw-pict piece dc x y))
 
 ;; (define (draw-pos row col sq-size dc)
 ;;   (define y (* row sq-size))
@@ -69,27 +59,23 @@
 ;;   (unless (char))
 ;;   (draw-piece ))
 
-(draw-board board-panel)
 
-;; (define my-canvas
-;;   (new canvas%
-;;        [parent board-panel]
-;;        [style '(transparent)]
-;;        [min-height (* square-size 8)]
-;;        [min-width (* square-size 8)]
-;;        [stretchable-height #f]
-;;        [stretchable-width #f]
-;;        [paint-callback
-;;         (lambda (canvas dc)
-;;           ;; (draw-board dc)
-;;            (define count 0)
-;;           (for ([p pieces])
-;;             (define-values (row col) (quotient/remainder count 8))
-;;             (define y (* row square-size))
-;;             (define x (* col square-size))
-;;             (unless (char-whitespace? p)
-;;               (draw-pict (char->piece p) dc x y))
-;;             (set! count (add1 count))))]))
+(define my-canvas
+  (new canvas%
+       [parent board-panel]
+       [style (list 'transparent)]
+       [min-height (* square-size 8)]
+       [min-width (* square-size 8)]
+       [stretchable-height #f]
+       [stretchable-width #f]
+       [paint-callback
+        (lambda (canvas dc)
+          (for-each (lambda (i)
+                      (define-values (row col) (index->row-col i))
+                      (define y (* row square-size))
+                      (define x (* col square-size))
+                      (draw-pict (list-ref (pos->squares pieces) i) dc x y))
+                    (build-list 64 identity)))]))
 
 (define message (new text-field% [parent control-panel]
                      [label #f]
@@ -103,23 +89,23 @@
                           [min-height 50]
                           [stretchable-height #f]))
 
-;; (new button%
-;;      [parent button-panel]
-;;      [label "<<"]
-;;      [callback (lambda (button event)
-;;                  (set! movenum (sub1 movenum))
-;;                  (set! pieces (list-ref states movenum))
-;;                  (send message set-value (number->string movenum))
-;;                  (send my-canvas refresh-now))])
+(new button%
+     [parent button-panel]
+     [label "<<"]
+     [callback (lambda (button event)
+                 (set! movenum (sub1 movenum))
+                 (set! pieces (list-ref states movenum))
+                 (send message set-value (number->string movenum))
+                 (send my-canvas refresh-now))])
 
-;; (new button%
-;;      [parent button-panel]
-;;      [label ">>"]
-;;      [callback (lambda (button event)
-;;                  (set! movenum (add1 movenum))
-;;                  (set! pieces (list-ref states movenum))
-;;                  (send message set-value (number->string movenum))
-;;                  (send my-canvas refresh-now))])
+(new button%
+     [parent button-panel]
+     [label ">>"]
+     [callback (lambda (button event)
+                 (set! movenum (add1 movenum))
+                 (set! pieces (list-ref states movenum))
+                 (send message set-value (number->string movenum))
+                 (send my-canvas refresh-now))])
 
 (new button%
      [parent button-panel]
@@ -127,14 +113,14 @@
      [callback (lambda (button event)
                  (connect message))])
 
-;; (new choice%
-;;      [parent button-panel]
-;;      [label #f]
-;;      [choices '("alpha" "line" "magnetic" "mark" "motif" "usual" "utrecht")]
-;;      [selection 5]
-;;      [callback (lambda (choice event)
-;;                  (define selection (send choice get-string-selection))
-;;                  (set! piece-style selection)
-;;                  (send my-canvas refresh-now))])
+(new choice%
+     [parent button-panel]
+     [label #f]
+     [choices '("alpha" "line" "magnetic" "mark" "motif" "usual" "utrecht")]
+     [selection 5]
+     [callback (lambda (choice event)
+                 (define selection (send choice get-string-selection))
+                 (set! piece-style selection)
+                 (send my-canvas refresh-now))])
 
 (send frame show #t)
