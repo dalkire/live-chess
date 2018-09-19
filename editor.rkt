f28be55c3fa3efc76ab9a57e825082d970875b31
David Alkire <david.alkire@gmail.com>
2018-09-19 08:36:44 -0400

    Trying to have a board background in editor

--- editor.rkt
+++ editor.rkt
@@ -13,14 +13,23 @@
 
 (define my-pb%
   (class pasteboard%
-    (inherit set-before)
+    (inherit set-before move-to)
     (super-new)
 
     (define (after-select snip on?)
-      (when on?
-        (set-before snip #f)))
+      (when (and on? (is-a? snip piece-snip%))
+          (set-before snip #f)))
 
-    (augment after-select)))
+    (define (after-move-to snip x y dragging?)
+      (unless dragging?
+          (printf "after-move-to (~a, ~a) ~a ~a\n" x y snip dragging?)
+          (print "is this snip a piece? ")
+          (println (is-a? snip piece-snip%))
+          (if (is-a? snip piece-snip%)
+              (move-to snip 400 400)
+              (move-to snip 0 0))))
+
+    (augment after-select after-move-to)))
 
 (define pb (new my-pb%))
 (send pb set-selection-visible #f)
@@ -28,10 +37,21 @@
 
 (define piece-snip%
   (class image-snip%
+    (inherit get-flags set-flags)
     (super-new)
 
+    ;; (define/override (on-event dc x y editorx editory event)
+    ;;   (when (send event button-up?)
+    ;;     (printf "button released (~a, ~a) (~a, ~a)\n" x y editorx editory)))
+
     (define/public (set-img path)
-      (send this set-bitmap (read-bitmap path)))))
+      (send this set-bitmap (read-bitmap path)))
+
+    (set-flags (cons 'handles-all-mouse-events (get-flags)))))
+
+(define board (new image-snip%))
+(send board set-bitmap (read-bitmap "./boards/board_pinstripes.png"))
+(println (send board get-flags))
 
 (define snip1 (new piece-snip%))
 (send snip1 set-img "/home/dalkire/programming/racket/live-chess/pieces/alpha/alpha_bb.png")
@@ -57,6 +77,7 @@
 (define snip8 (new piece-snip%))
 (send snip8 set-img "/home/dalkire/programming/racket/live-chess/pieces/alpha/alpha_bp.png")
 
+
 (send pb insert snip1 0 0)
 (send pb insert snip2 80 0)
 (send pb insert snip3 160 0)
@@ -66,4 +87,8 @@
 (send pb insert snip7 480 0)
 (send pb insert snip8 560 0)
 
+(send pb insert board 0 0)
+
 (send frame show #t)
+
+(define bdc (new bitmap-dc% [bitmap (make-object bitmap% "./boards/board_pinstripes.png")]))
