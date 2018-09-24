64d60acf3c2ac708a8ddf04b603a17c6791a5f12
David Alkire <david.alkire@gmail.com>
2018-09-24 09:10:45 -0400

    Added square->index function

--- lib.rkt
+++ lib.rkt
@@ -1,5 +1,7 @@
 #lang typed/racket
 
+(define POS "RNBQKB RPPPP PPP     N      P      p        p   ppp  ppprnbqkbnr")
+
 (provide (struct-out pt)
          (struct-out coord))
 
@@ -24,6 +26,9 @@
                              "a3" "b3" "c3" "d3" "e3" "f3" "g3" "h3"
                              "a2" "b2" "c2" "d2" "e2" "f2" "g2" "h2"
                              "a1" "b1" "c1" "d1" "e1" "f1" "g1" "h1"))
+
+(define-type Piece (U 'wp 'wn 'wb 'wr 'wq 'wk
+                      'bp 'bn 'bb 'br 'bq 'bk))
 
 (define-type RowOrColumn (U 1 2 3 4 5 6 7 8))
 (define-type Column RowOrColumn)
@@ -119,3 +124,13 @@
                    (pt-part->square-index (pt-y p) square-size))))
     (square-string->square
      (column-row-chars->square-string column-char row-char))))
+
+;; (: square->piece (-> Square String Piece))
+;; (define (square->piece sq board-string))
+
+
+(: square->index (-> Square Integer))
+(define (square->index sq)
+  (+
+   (* (- (square->row sq) 1) 8)
+   (- (square->column sq) 1)))
